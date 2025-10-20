7602f48b6b1258ec60e54768e758e698b8117b71
tw660710709 <thanakrit.w2005@gmail.com>
2025-10-20 14:27:58 +0000

    week7

---
+++ week7-lab4/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"database/sql"
+	_"github.com/lib/pq"
+	"log"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+var db *sql.DB
+
+func initDB() {
+	var err error
+	host := getEnv("DB_HOST", "")
+	name := getEnv("DB_NAME", "")
+	user :=  getEnv("DB_USER", "")
+	password := getEnv("DB_PASSWORD", "")
+	port := getEnv("DB_PORT", "")
+
+	conSt := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, name)
+	//fmt.Println(conSt)
+
+	db, err = sql.Open("postgres", conSt)
+	if err != nil {
+		log.Fatal("failed to open database ", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("failed to connect database ", err)
+	}
+
+	log.Println("successfully connected to database")
+
+}
+
+func main() {
+	initDB()
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context){
+		err := db.Ping()
+		if err != nil {
+			c.JSON(http.StatusServiceUnavailable, gin.H{"message":"unhealhty", "error":err})
+			return
+		}
+		c.JSON(200, gin.H{"message" : "healthy"})
+	})
+
+	// api := r.Group("/api/v1")
+	// {
+	// 	api.GET("/books", getBooks)
+	// 	api.GET("/books/:id", getBook)
+	// 	api.POST("/books", createBook)
+	// 	api.PUT("/books/:id", updateBook)
+	// 	api.DELETE("/books/:id", deleteBook)
+	// }
+
+	r.Run(":8080")
+}
+
+//go get github.com/lib/pq
